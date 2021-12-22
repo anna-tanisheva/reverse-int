138d1b0e195dcc9c4ea30a4f5158b1d2702dfdf1
Anna Tanisheva <anna-tanisheva@yandex.ru>
2021-12-22 18:30:45 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let numberArrReversed = String(n).split('').reverse();
+    if (numberArrReversed[0] !== '0' && numberArrReversed[numberArrReversed.length - 1] !== '-') {
+        return Number(numberArrReversed.join(''));
+    } else if (numberArrReversed[0] === '0') {
+        numberArrReversed.shift();
+        return Number(numberArrReversed.join(''))
+    } else if (numberArrReversed[numberArrReversed.length - 1] === '-') {
+        numberArrReversed.pop();
+        return Number(numberArrReversed.join(''))
+    }
 }
